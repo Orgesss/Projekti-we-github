c40072f835753f8c63e95153fc085c3c878c1516
Orgesss <orgesiav@gmail.com>
2024-01-25 17:23:53 +0100

    users.php

---
+++ users.php
@@ -0,0 +1,27 @@
+<?php
+  $user1 = [
+    "name"=>"Orges",
+    "surname"=>"Avdiu",
+    "email"=>"oa@gmail.com",
+    "password" => "Orgesi123",
+    "role"=>"admin"
+  ];
+  
+  $user2 = [
+    "name"=>"Dren",
+    "surname"=>"Binishi",
+    "email"=>"db@gmail.com",
+    "password" => "Dreni321",
+    "role"=>"user"
+  ];
+
+  $user3 = [
+    "name"=>"Lum",
+    "surname"=>"Peta",
+    "email"=>"lp@ubt-uni.net",
+    "password" => "lumi123",
+    "role"=>"user"
+  ];
+
+  $users = [$user1, $user2, $user3];
+?>
