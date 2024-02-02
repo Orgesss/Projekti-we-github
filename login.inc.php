871c584273ea6775ae570edb5a015ebe353e7dd6
Orgesss <orgesiav@gmail.com>
2024-02-02 01:14:41 +0100

    DREN BINISHI login.inc.php

---
+++ login.inc.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+if(isset($_POST["submit"])) 
+ {
+ 
+  $uid = $_POST["uid"];
+  $pwd = $_POST["pwd"];
+
+
+include "../Projekti-we-github/dbh.classes.php";
+include "../Projekti-we-github/login.classes.php";
+include "../Projekti-we-github/login-contr.classes.php";
+ $login = new LoginContr($uid, $pwd);
+$login->loginUser();
+
+header("location: home.php?loginsuccess=true");
+
+ }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sign-in</title>
+    <link rel="stylesheet" href="Signin.css" />
+   
+  </head>
+  <body>
+    <header>
+      <img src="nike.logo.png" alt="" height="45px" />
+      <nav>
+        <ul>
+          <li><a class="a2" href="Home.php">Home</a></li>
+          <li><a class="a2" href="News.html">News</a></li>
+          <li><a class="a2" href="Contact.html">Contact us</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main class="login-container">
+      <h1>Log In</h1>
+      <form class="login-form" method="post">
+        <label for="text">Username</label>
+        <input type="text" name="uid" placeholder="Username" required />
+
+        <label for="password">Password:</label>
+        <input type="password" name="pwd" placeholder="Password" required />
+
+        <button type="submit" name="submit">Log in</button>
+      </form>
+  
+      <div class="welcome-message">
+        <p><strong>Kycuni ne llogarine tuaj</strong></p>
+        <p>Nuk keni llogari? <a href="signup.inc.php">Kycuni ketu</a>.</p>
+      </div>
+    </main>
+  </body>
+  </html>
